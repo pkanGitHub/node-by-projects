b0f898853b28b275e869e8c1cf6a502c5c5abd61
Phyllis Kan <pyk9q7@gmail.com>
2020-10-08 14:23:34 -0500

    add note and test to see duplicates

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,7 +1,6 @@
 const chalk = require('chalk')
-const { demandOption } = require('yargs')
 const yargs = require('yargs')
-const getNotes = require('./notes.js')
+const notes = require('./notes.js')
 
 // Customize yargs version
 // yargs.version('1.1.0')
@@ -23,8 +22,7 @@ yargs.command({
         }
     },
     handler: function (argv) {
-        console.log('Title: ' + argv.title)
-        console.log('Body: ' + argv.body)
+        notes.addNote(argv.title, argv.body)
     }
 })
 
