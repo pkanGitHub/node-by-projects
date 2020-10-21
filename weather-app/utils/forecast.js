ea7e33733f8f5b62f381b5b1d16e09766e8231bd
Phyllis Kan <pyk9q7@gmail.com>
2020-10-21 14:39:26 -0500

    refactor weather app with destructuring

--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -5,14 +5,14 @@ const forecast = (latitude, longitude, callback) => {
     //const url = 'http://api.weatherstack.com/current?access_key=c27520457bc019e0fe6ad9870dcaf22c&query=37.8267,-122.4233&units=f' // for fahrenheit 
 
 
-    request({ url: url, json: true }, (error, response) => {
+    request({ url: url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to weather services!', undefined)
-        } else if (response.body.error) {
+        } else if (body.error) {
             callback('Unable to find the location.', undefined)
         } else {
             callback(undefined,
-                response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature + ' degree out. It feels like ' + response.body.current.feelslike + ' degree out.'
+                body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degree out. It feels like ' + body.current.feelslike + ' degree out.'
             )
         }
     })
