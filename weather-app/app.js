3eb2711deb2f31362ba250f183ff49ab06e6190e
Phyllis Kan <pyk9q7@gmail.com>
2020-10-21 13:54:34 -0500

    using yargs to print out weather

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,13 +2,28 @@
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
+const address = process.argv[2]
 
-geocode('Boston', (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-    forecast(data.latitude, data.longitude, (error, data) => {
-        console.log('Error', error)
-        console.log('Data', data)
+if (!address) {
+    console.log('Please provide an address')
+} else {
+    geocode(address, (error, data) => {
+        if (error) {
+            return console.log(error)
+        }
+
+        forecast(data.latitude, data.longitude, (error, forecastData) => {
+            if (error) {
+                return console.log(error)
+            }
+            console.log(data.location)
+            console.log(forecastData)
+        })
     })
-})
+}
+
+console.log(process.argv)
+
+
+
 
