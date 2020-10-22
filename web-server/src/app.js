83f55408f96c88a7fc7e96b66b8d12319b652f03
Phyllis Kan <pyk9q7@gmail.com>
2020-10-22 15:21:06 -0500

    replace static html into dynamic html using hbs

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -6,7 +6,26 @@ const app = express()
 app.set('view engine', 'hbs')
 app.use(express.static(path.join(__dirname, '../public')))
 
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Sora'
+    })
+})
 
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Sora'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help page',
+        description: 'Whatever you need help with'
+    })
+})
 app.get('/weather', (req, res) => {
     res.send('Hello from weather page!')
 })
