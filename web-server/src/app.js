81d4b6a0c11b735dc6c0cabe3964a78ff83b94ca
Phyllis Kan <pyk9q7@gmail.com>
2020-11-12 20:27:02 -0600

    intro to query string

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -35,7 +35,28 @@ app.get('/help', (req, res) => {
     })
 })
 app.get('/weather', (req, res) => {
-    res.send('Hello from weather page!')
+    if (!req.query.address) {
+        return res.send({
+            error: 'You must provide an address'
+        })
+    }
+    res.send({
+        forecast: 'It is snowing',
+        location: 'Philadelphia',
+        address: req.query.address
+    })
+})
+
+app.get('/products', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term'
+        })
+    }
+    console.log(req.query.search)
+    res.send({
+        products: []
+    })
 })
 
 app.get('/help/*', (req, res) => {
