45a15ae241505ee5a451e784d559ec2e9afeeeb1
Phyllis Kan <pyk9q7@gmail.com>
2020-11-12 21:14:57 -0600

    coporate weather api into the app using request

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,6 +2,9 @@ const path = require('path')
 const express = require('express')
 const hbs = require('hbs')// for partials
 
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
 const app = express()
 
 const viewsPath = path.join(__dirname, '../templates/views') //use this when your views folder isn't call views
@@ -40,11 +43,28 @@ app.get('/weather', (req, res) => {
             error: 'You must provide an address'
         })
     }
-    res.send({
-        forecast: 'It is snowing',
-        location: 'Philadelphia',
-        address: req.query.address
+
+    geocode(req.query.address, (error, { latitude, longitude, location }) => {
+        if (error) {
+            return res.send({ error })
+        }
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({ error })
+            }
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        })
     })
+
+    // res.send({
+    //     forecast: 'It is snowing',
+    //     location: 'Philadelphia',
+    //     address: req.query.address
+    // })
 })
 
 app.get('/products', (req, res) => {
