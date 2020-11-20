504039418b3ca56d2537d4d97c77e87bef8ecfbf
Phyllis Kan <pyk9q7@gmail.com>
2020-11-20 14:38:24 -0600

    create patch for task using async

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -65,6 +65,9 @@ app.patch('/users/:id', async (req, res) => {
     }
 })
 
+
+
+
 app.post('/tasks', async (req, res) => {
     const task = new Task(req.body)
 
@@ -99,6 +102,29 @@ app.get('/tasks/:id', async (req, res) => {
         res.status(500).send()
     }
 })
+
+app.patch('/tasks/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowUpdates = ['description', 'completed']
+    const isValidOperation = updates.every((update) => {
+        return allowUpdates.includes(update) // return true
+    })
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!' })
+    }
+
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+        if (!task) {
+            return res.status(404).send()
+        }
+        res.send(task)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
 
 app.listen(port, () => {
     console.log('Server is on port ' + port)
