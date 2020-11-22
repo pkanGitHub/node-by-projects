742fbc6bf94889297009db9ffa4788690cbdbba1
Phyllis Kan <pyk9q7@gmail.com>
2020-11-22 15:02:21 -0600

    add Auth token to user

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -39,9 +40,28 @@ const userSchema = new mongoose.Schema({
                 throw new Error('Age must be a positive number')
             }
         } // custom validator
-    }
+    },
+    //for logging out
+    tokens: [{
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 })
+// methods are accessible on instances (instance method)
+userSchema.methods.generateAuthToken = async function () {
+    const user = this // not necessary
+    const token = jwt.sign({ _id: user._id.toString() }, 'thisismynewcourse')
 
+    user.tokens = user.tokens.concat({ token: token })
+    await user.save()
+
+    return token
+
+}
+
+// statics code are accessible on model (model methods)
 userSchema.statics.findByCredentials = async (email, password) => {
     const user = await User.findOne({ email: email })
 
