bc9eb721e01e82ef928b2e752b13befaed71dd6f
Phyllis Kan <pyk9q7@gmail.com>
2020-12-04 18:42:58 -0600

    add validation for file upload & handle Exp. Err

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -9,11 +9,26 @@ const port = process.env.PORT || 3000
 
 const multer = require('multer')
 const upload = multer({
-    dest: 'images'
+    dest: 'images',
+    limits: {
+        fileSize: 1000000 //in B so 1m for MB
+    },
+    fileFilter(req, file, cb) {
+        // if (!file.originalname.endsWith('.pdf')) {
+        // return cb(new Error('Please upload a PDF'))
+        // OR (below)
+        if (!file.originalname.match(/\.(doc|docx)$/)) {
+            return cb(new Error('Please upload a Word Document'))
+        }
+
+        cb(undefined, true)
+    }
 })
 
 app.post('/upload', upload.single('upload'), (req, res) => {
     res.send()
+}, (error, req, res, next) => {
+    res.status(400).send({ error: error.message })
 })
 
 app.use(express.json()) //automatically parse incoming json to an object
