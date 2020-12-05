5706bdce075e2e5c0a21a9276a890dcb075621ac
Phyllis Kan <pyk9q7@gmail.com>
2020-12-04 19:12:59 -0600

    remove temporary multer request

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -7,29 +7,6 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-const multer = require('multer')
-const upload = multer({
-    dest: 'images',
-    limits: {
-        fileSize: 1000000 //in B so 1m for MB
-    },
-    fileFilter(req, file, cb) {
-        // if (!file.originalname.endsWith('.pdf')) {
-        // return cb(new Error('Please upload a PDF'))
-        // OR (below)
-        if (!file.originalname.match(/\.(doc|docx)$/)) {
-            return cb(new Error('Please upload a Word Document'))
-        }
-
-        cb(undefined, true)
-    }
-})
-
-app.post('/upload', upload.single('upload'), (req, res) => {
-    res.send()
-}, (error, req, res, next) => {
-    res.status(400).send({ error: error.message })
-})
 
 app.use(express.json()) //automatically parse incoming json to an object
 
